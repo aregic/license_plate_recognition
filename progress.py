9b921c8d33451c499bb4d44981582b1149019865
aregic <aregic>
2017-11-27 01:15:13 +0100

    not working yet

---
+++ progress.py
@@ -0,0 +1,30 @@
+import numpy as np
+import scipy.ndimage
+import pandas as pd
+import matplotlib.pyplot as plt
+from inspect_images import *
+from nn import *
+from tensorflow.python import debug as tf_debug
+
+
+def doit():
+    stats = pd.DataFrame.from_csv("./stats.csv")
+    smallest = stats[stats["x"]==stats["x"].min()]
+    picloc = "./samples/" + smallest["file name"].values[0]
+    pic = scipy.ndimage.imread(picloc)
+    label = get_bounding_box(picloc) 
+    sess = tf.Session()
+    #sess = tf_debug.LocalCLIDebugWrapperSession(sess)
+
+    with sess.as_default():                                                        
+       respic, reslabel = tf_pad_image(pic, list(label.astype("int32").flat), 512,512)
+       respic = respic.eval()
+       reslabel = reslabel
+       reslabel = np.asarray( [r.eval() for r in reslabel] )
+       reslabel = reslabel.reshape((4,2))
+
+    return respic, reslabel, label
+
+pic, label, origlabel = doit()
+print("Result: %s, %s, %s" % doit())
+draw_bounding_box(pic, label)
